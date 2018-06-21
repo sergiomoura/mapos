823d81f32ff87aa3a5254a7c92c980324d882d09
Sérgio Moura <smouracalmon@gmail.com>
2018-06-21 18:17:34 -0300

    Melhoramentos no desenho da tela de login

--- www/webapp/src/app/login/login.component.ts
+++ www/webapp/src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-  login: {username:string, password:string} = {username:'',password:''};  
+  login: {u:string, p:string} = {u:'',p:''};  
 
   constructor() { }
 
