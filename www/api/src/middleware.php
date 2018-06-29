906cc943694a2793523dc90ae7ffc2fb05693534
Sérgio Moura <smouracalmon@gmail.com>
2018-06-29 07:15:07 -0300

    Adicionado middleware que checa o token do usuário antes da execução na api

--- www/api/src/middleware.php
+++ www/api/src/middleware.php
@@ -1,4 +1,72 @@
 <?php
-// Application middleware
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+// Application middleware
+class CheckAuthMiddleware
+{
+    /**
+     * Example middleware invokable class
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+
+     // Construtor que permite que container do app seja injetado na classe middleware
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    public function __invoke($request, $response, $next)
+    {
+        // Capturando vetor de headers
+        $headers = $request->getHeaders();
+        
+        // Se for requisição OPTIONS com Access-Control-Request-Method no header, deixa passar
+        if(array_key_exists('HTTP_ACCESS_CONTROL_REQUEST_METHOD',$headers) && $request->getMethod() == 'OPTIONS'){
+        
+            $response = $next($request, $response);
+            return $response->withStatus(201);
+        }
+        
+        if (array_key_exists('HTTP_AUTHORIZATION', $headers)){
+            
+            // Capturando o token do header HTTP_AUTHORIZATION;
+            $token = str_replace('Bearer ','',$request->getHeaders()['HTTP_AUTHORIZATION'][0]);
+
+            // Consultando validade do token
+            $sql = 'SELECT id FROM maxse_usuarios WHERE token=:token AND validade_do_token>NOW()';
+            $stmt = $this->container->db->prepare($sql);
+            $stmt->execute(array(':token'=>$token));
+            $id = $stmt->fetch();
+
+            if($id === false) {
+                return $response->withStatus(401);
+            } else {
+                $response = $next($request, $response);
+                return $response;
+            }
+            
+        } else {
+            
+            // Checando se é uma tentativa de login
+            if($request->getUri()->getPath() != '/login'){
+
+                // Não é login. Retornando com status 401
+                $response = $next($request, $response);
+                return $response->withStatus(401);
+
+            } else {
+
+                // É uma tentativa de login. Prosseguindo
+                $response = $next($request, $response);
+                return $response;
+
+            }
+        }
+    }
+}
+
+$app->add(new CheckAuthMiddleware($app->getContainer()));
+
