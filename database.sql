417055267a419cb1bb5f48225a518e42f8689b4c
Sérgio Moura <smouracalmon@gmail.com>
2018-07-22 15:54:41 -0300

    Tela de tarefas desenhada

--- database.sql
+++ database.sql
@@ -345,7 +345,7 @@ CREATE TABLE `maxse_medidas_area` (
   PRIMARY KEY (`id`),
   KEY `fk_maxse_medidas_area_1_idx` (`id_sse`),
   CONSTRAINT `fk_maxse_medidas_area_1` FOREIGN KEY (`id_sse`) REFERENCES `maxse_sses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -354,7 +354,7 @@ CREATE TABLE `maxse_medidas_area` (
 
 LOCK TABLES `maxse_medidas_area` WRITE;
 /*!40000 ALTER TABLE `maxse_medidas_area` DISABLE KEYS */;
-INSERT INTO `maxse_medidas_area` VALUES (34,3.50,2.20,16,'p');
+INSERT INTO `maxse_medidas_area` VALUES (34,3.50,2.20,16,'p'),(35,1.00,3.00,23,'p'),(36,2.00,1.00,23,'p'),(37,2.00,2.00,24,'p');
 /*!40000 ALTER TABLE `maxse_medidas_area` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -517,7 +517,7 @@ CREATE TABLE `maxse_sses` (
   KEY `fk_maxse_sses_2_idx` (`status`),
   CONSTRAINT `fk_maxse_sses_1` FOREIGN KEY (`id_bairro`) REFERENCES `maxse_bairros` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_maxse_sses_2` FOREIGN KEY (`status`) REFERENCES `maxse_sse_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -526,7 +526,7 @@ CREATE TABLE `maxse_sses` (
 
 LOCK TABLES `maxse_sses` WRITE;
 /*!40000 ALTER TABLE `maxse_sses` DISABLE KEYS */;
-INSERT INTO `maxse_sses` VALUES (16,'Rua Décio Andrade Silva, 356',153,'3652359',1,'2018-07-15 10:06:06','2018-05-31 10:26:00',1,NULL,0,-22.9533354,-47.1839279);
+INSERT INTO `maxse_sses` VALUES (16,'Rua Décio Andrade Silva, 356',153,'3652359',1,'2018-07-15 10:06:06','2018-05-31 10:26:00',1,NULL,0,-22.9533354,-47.1839279),(22,'Rua Luis Marcelino Guemeli,30',10,'3652380',2,'2018-07-22 08:30:00','2018-07-21 16:20:00',NULL,'',0,-22.0000000,-47.0000000),(23,'Rua Barreto Leme 100',582,'333333',2,'2018-07-22 15:28:30','2018-07-22 00:00:00',0,NULL,0,-43.0000000,-22.0000000),(24,'Av Lix da Cunha, 1200',582,'55555',3,'2018-07-22 15:29:57','2018-07-22 00:00:00',1,'werwer rwewre ',0,-43.0000000,-22.0000000);
 /*!40000 ALTER TABLE `maxse_sses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -719,7 +719,7 @@ CREATE TABLE `maxse_usuarios` (
 
 LOCK TABLES `maxse_usuarios` WRITE;
 /*!40000 ALTER TABLE `maxse_usuarios` DISABLE KEYS */;
-INSERT INTO `maxse_usuarios` VALUES (1,'root','$1$isThvBp0$1zlwWhFhQDLckghROi5qj0','5b5489f20df585.10086572','2018-07-22 11:43:14',1,1,1,1),(45,'joao','$1$AmpuA.Jb$oOG7ujtbU0ZW9.tiiiEOb1',NULL,NULL,1,0,1,123),(46,'jupter','$1$xd9ZZwfy$0iLNsYRrExY7OSrwWcu.r/',NULL,NULL,1,0,1,124),(47,'pedro','$1$nRTM0bJi$81x6VWMIAaDDs3EpsH.Lv0',NULL,NULL,1,0,1,125),(48,'maxwell','$1$08EoUozo$ucrdOaS1lh3c8YxmA01iS.',NULL,NULL,1,0,1,126),(49,'edson','$1$P9silUJQ$0qJLE9fEUtilVlwTDzEVe/','5b4c604f52f523.99954247','2018-07-16 07:07:27',1,0,1,127),(62,'michel','$1$9WZEShzZ$PjX62Fls8w3EjVxyBbfA4.',NULL,NULL,0,0,1,146),(64,'ddasad','$1$n.MUb9yL$r5/xDxhxXjvy9XvW5DnPM/',NULL,NULL,0,0,1,151),(66,'ssssss','$1$iYEEprBt$HDtmLf6njU552lHX3YIL4.',NULL,NULL,2,0,1,155),(67,'asdasda','$1$GTzKWy2A$XCesYurRUTufgqNr1nLzK0',NULL,NULL,0,0,1,157),(73,'boulos','$1$uMvLp/7c$WSOacfAme8FNMZM2xiT4..',NULL,NULL,0,0,1,176),(78,'afranio','$1$Mwp0X7Q3$46XHfnWszL9cCwrAKx8sX0','5b5488cf373888.91643667','2018-07-22 11:38:23',2,0,1,184);
+INSERT INTO `maxse_usuarios` VALUES (1,'root','$1$isThvBp0$1zlwWhFhQDLckghROi5qj0','5b54cdae0ff410.39193865','2018-07-22 16:32:14',1,1,1,1),(45,'joao','$1$AmpuA.Jb$oOG7ujtbU0ZW9.tiiiEOb1',NULL,NULL,1,0,1,123),(46,'jupter','$1$xd9ZZwfy$0iLNsYRrExY7OSrwWcu.r/',NULL,NULL,1,0,1,124),(47,'pedro','$1$nRTM0bJi$81x6VWMIAaDDs3EpsH.Lv0',NULL,NULL,1,0,1,125),(48,'maxwell','$1$08EoUozo$ucrdOaS1lh3c8YxmA01iS.',NULL,NULL,1,0,1,126),(49,'edson','$1$HNiLkI9h$7RIsvB8HOegaryWDzTFhh/','5b54cb838917f7.10407155','2018-07-22 16:22:59',1,0,1,127),(62,'michel','$1$9WZEShzZ$PjX62Fls8w3EjVxyBbfA4.',NULL,NULL,0,0,1,146),(64,'ddasad','$1$n.MUb9yL$r5/xDxhxXjvy9XvW5DnPM/',NULL,NULL,0,0,1,151),(66,'ssssss','$1$iYEEprBt$HDtmLf6njU552lHX3YIL4.',NULL,NULL,2,0,1,155),(67,'asdasda','$1$GTzKWy2A$XCesYurRUTufgqNr1nLzK0',NULL,NULL,0,0,1,157),(73,'boulos','$1$uMvLp/7c$WSOacfAme8FNMZM2xiT4..',NULL,NULL,0,0,1,176),(78,'afranio','$1$rot279Dz$tRlNynKSV.090b9KoNL3X0','5b54cb7681b8d5.65101120','2018-07-22 16:22:46',2,0,1,184);
 /*!40000 ALTER TABLE `maxse_usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -750,4 +750,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-22 10:43:55
+-- Dump completed on 2018-07-22 15:54:42
