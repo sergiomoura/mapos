e20cb845cf5e36ffa4d73b66d4db3a3054f7b1db
Sérgio Moura <smouracalmon@gmail.com>
2018-10-20 07:06:45 -0300

    Guardando busca de sses no localstorage

--- www/webapp/src/app/_components/buscador-modal/buscador-modal.component.ts
+++ www/webapp/src/app/_components/buscador-modal/buscador-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { MatDialogRef, MatSnackBar } from '@angular/material';
 import { DomasasService } from 'src/app/_services/domasas.service';
 import { Bairro } from 'src/app/_models/bairro';
@@ -10,14 +10,21 @@ import { Busca } from 'src/app/_models/busca';
 import { EventsService } from 'src/app/_services/events.service';
 import { SsesService } from 'src/app/_services/sses.service';
 import { SSE } from 'src/app/_models/sse';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
-enum Prioridade {
+enum ChavesDeBusca {
+	MAPA = 'busca_mapa',
+	GRID = 'busca_grid'
+}
+
+enum enum_prioridade {
 	NORMAL = '0',
 	PRIORIDADE = '1',
 	URGENTE = '2'
 }
 
-enum Status {
+enum enum_status {
 	CANCELADA	= 'CANCELADA',
 	RETRABALHO	= 'RETRABALHO',
 	DIVERGENTE	= 'DIVERGENTE',
@@ -26,14 +33,17 @@ enum Status {
 	EXECUTANDO	= 'EXECUTANDO',
 	PENDENTE	= 'PENDENTE',
 	FINALIZADA	= 'FINALIZADA'
-}
+};
 
 class ElementoSelecionavel {
-	elemento:Equipe | Prioridade | string;
+	elemento:Equipe | enum_prioridade | enum_status;
 	selecionado:boolean;
 }
 
-class ParametrosDeBusca implements Busca {
+@Injectable({
+	providedIn: 'root'
+})
+class ParametrosDeBusca {
 	
 	public equipesSelecionaveis:ElementoSelecionavel[] = [];
 	public prioridadesSelecionaveis:ElementoSelecionavel[] = [];
@@ -42,79 +52,139 @@ class ParametrosDeBusca implements Busca {
 	public agendadas_ate?: Date = null;
 	public realizadas_de?: Date = null;
 	public realizadas_ate?: Date = null;
-	id_fechamento?: number = null;
+	public id_fechamento?: number = null;
 
-	constructor(){
+	constructor(
+		private equipesService:EquipesService,
+		private router:Router
+	){
+		
+		// Definindo local da busca no localStorage
+		let chave:string;
+		if(this.router.url == '/home/sses/grid') {
+			chave = ChavesDeBusca.GRID;
+		} else {
+			chave = ChavesDeBusca.MAPA;
+		}
+
+		// Carregando busca a do local storage
+		let strBusca = localStorage.getItem(chave);
+		let busca: Busca;
+		if(strBusca) {
+			busca = <Busca>JSON.parse(strBusca);
+		}
+
+		
 
 		// Criando vetor de prioridades selecionaveis
-		for (let i in Prioridade) {
+		for (let i in enum_prioridade) {
+			
+			let selecionado:boolean;
+			if(busca){
+				selecionado = busca.prioridades.findIndex(
+					(p) => {
+						return p == +<enum_prioridade>enum_prioridade[i];
+					}
+				) != -1;
+			} else {
+				selecionado = true;
+			}
+
 			this.prioridadesSelecionaveis.push(
 				{
-					'elemento': Prioridade[i],
-					'selecionado': true
+					'elemento': <enum_prioridade>enum_prioridade[i],
+					'selecionado': selecionado
 				}
 			);
+
 		}
 
 		// Criando vetor de status selecionaveis
-		for (let i in Status) {
+		for (let i in enum_status) {
+
+			let selecionado:boolean;
+			if(busca) {
+				selecionado = busca.status.findIndex(
+					(p) => {
+						return p == <enum_status>enum_status[i];
+					}
+				) != -1;
+			} else {
+				selecionado = true;
+			}
+
 			this.statusSelecionaveis.push(
 				{
-					'elemento': Status[i],
-					'selecionado': true
+					'elemento': <enum_status>enum_status[i],
+					'selecionado': selecionado
 				}
 			);
-		}
-	}
 
-	setEquipes(equipes:Equipe[]):void{
-		// Criando vetor de equipes selecionaveis
-		for (let i = 0; i < equipes.length; i++) {
-			this.equipesSelecionaveis.push(
-				{
-					'elemento': equipes[i],
-					'selecionado': true
+		}
+
+		// Criando vetor de equipes selecionáveis
+		equipesService.getEquipes().subscribe(
+			(equipes) => {
+				
+				for (let i = 0; i < equipes.length; i++) {
+
+					let selecionado:boolean;
+					if(busca) {
+						selecionado = busca.equipes.findIndex(
+							(equipe) => {
+								return equipes[i].id == equipe.id;
+							}
+						) != -1;
+					} else {
+						selecionado = true;
+					}
+
+					this.equipesSelecionaveis.push(
+						{
+							elemento: equipes[i],
+							selecionado: selecionado
+						}
+					);
 				}
-			);
+			}
+		)
+	}
+
+	public get busca() : Busca {
+		return {
+			agendadas_ate: this.agendadas_ate,
+			agendadas_de: this.agendadas_de,
+			realizadas_ate: this.realizadas_ate,
+			realizadas_de: this.realizadas_de,
+			equipes: this.equipesSelecionaveis.filter(
+				(es) => {
+					return es.selecionado
+				}
+			).map(
+				(es) => {
+					return (<Equipe>es.elemento);
+				}
+			),
+			prioridades: this.prioridadesSelecionaveis.filter(
+				(es) => {
+					return es.selecionado
+				}
+			).map(
+				(es) => {
+					return +<enum_prioridade>es.elemento;
+				}
+			),
+			status: this.statusSelecionaveis.filter(
+				(es) => {
+					return es.selecionado
+				}
+			).map(
+				(es) => {
+					return <enum_status>es.elemento;
+				}
+			)
 		}
 	}
-	
-	public get equipes() : Equipe[] {
-		return this.equipesSelecionaveis.filter(
-			e => {
-				return e.selecionado;
-			}
-		).map(
-			e => {
-				return <Equipe>e.elemento;
-			}
-		)
-	}
-
-	public get status() : Status[] {
-		return this.statusSelecionaveis.filter(
-			e => {
-				return e.selecionado;
-			}
-		).map(
-			e => {
-				return <Status>e.elemento;
-			}
-		)
-	}
-
-	public get prioridades() : number[] {
-		return this.prioridadesSelecionaveis.filter(
-			e => {
-				return e.selecionado;
-			}
-		).map(
-			e => {
-				return +<Prioridade>e.elemento;
-			}
-		)
-	}
-
 }
 
 @Component({
@@ -124,88 +194,52 @@ class ParametrosDeBusca implements Busca {
 })
 export class BuscadorModalComponent implements OnInit {
 
-	public bairros:Bairro[];
-	public tdss:TipoDeServico[];
-	public equipes:Equipe[];
-	public busca:ParametrosDeBusca;
 	public buscando:boolean;
 
 	constructor(
 		public dialogRef: MatDialogRef<BuscadorModalComponent>,
-		private domasasService:DomasasService,
-		private tdsService:TiposDeServicoService,
-		private equipesService:EquipesService,
-		private evtService:EventsService,
 		private ssesService:SsesService,
-		private snackBar:MatSnackBar
-	) { 
-		this.busca = new ParametrosDeBusca();
-	}
+		private snackBar:MatSnackBar,
+		private parametrosDeBusca:ParametrosDeBusca,
+		private router:Router
+	) { }
 
 	ngOnInit() {
-		this.getBairros();
-		this.getTdss();
-		this.getEquipes();
 	}
-
-	private getBairros():void{
-		this.domasasService.getFlat().subscribe(
-			(bairros) => {
-				this.bairros = bairros;
-			}
-		)
-	}
-
-	private getTdss():void{
-		this.tdsService.get().subscribe(
-			(tdss) => {
-				this.tdss = tdss;
-			}
-		)
-	}
-
-	private getEquipes():void{
-		this.equipesService.getEquipes().subscribe(
-			(equipes) => {
-				this.equipes = equipes;
-				this.busca.setEquipes(this.equipes);
-			}
-		)
-	}
-
+	
 	onMTEquipesClick():void {
-		for (let index = 0; index < this.busca.equipesSelecionaveis.length; index++) {
-			this.busca.equipesSelecionaveis[index].selecionado = true;
+		for (let index = 0; index < this.parametrosDeBusca.equipesSelecionaveis.length; index++) {
+			this.parametrosDeBusca.equipesSelecionaveis[index].selecionado = true;
 		}
 	}
 
 	onDMTEquipesClick():void {
-		for (let index = 0; index < this.busca.equipesSelecionaveis.length; index++) {
-			this.busca.equipesSelecionaveis[index].selecionado = false;
+		for (let index = 0; index < this.parametrosDeBusca.equipesSelecionaveis.length; index++) {
+			this.parametrosDeBusca.equipesSelecionaveis[index].selecionado = false;
 		}
 	}
 
 	onMTStatusClick():void {
-		for (let index = 0; index < this.busca.statusSelecionaveis.length; index++) {
-			this.busca.statusSelecionaveis[index].selecionado = true;
+		for (let index = 0; index < this.parametrosDeBusca.statusSelecionaveis.length; index++) {
+			this.parametrosDeBusca.statusSelecionaveis[index].selecionado = true;
 		}
 	}
 
 	onDMTStatusClick():void {
-		for (let index = 0; index < this.busca.statusSelecionaveis.length; index++) {
-			this.busca.statusSelecionaveis[index].selecionado = false;
+		for (let index = 0; index < this.parametrosDeBusca.statusSelecionaveis.length; index++) {
+			this.parametrosDeBusca.statusSelecionaveis[index].selecionado = false;
 		}
 	}
 
 	onMTPrioridadesClick():void {
-		for (let index = 0; index < this.busca.prioridadesSelecionaveis.length; index++) {
-			this.busca.prioridadesSelecionaveis[index].selecionado = true;
+		for (let index = 0; index < this.parametrosDeBusca.prioridadesSelecionaveis.length; index++) {
+			this.parametrosDeBusca.prioridadesSelecionaveis[index].selecionado = true;
 		}
 	}
 
 	onDMTPrioridadesClick():void {
-		for (let index = 0; index < this.busca.prioridadesSelecionaveis.length; index++) {
-			this.busca.prioridadesSelecionaveis[index].selecionado = false;
+		for (let index = 0; index < this.parametrosDeBusca.prioridadesSelecionaveis.length; index++) {
+			this.parametrosDeBusca.prioridadesSelecionaveis[index].selecionado = false;
 		}
 	}
 
@@ -223,13 +257,26 @@ export class BuscadorModalComponent implements OnInit {
 		this.buscando = true;
 
 		// Fazendo requisição
-		this.ssesService.getAll(this.busca).subscribe(
+		let busca:Busca = this.parametrosDeBusca.busca;
+		this.ssesService.getAll(busca).subscribe(
 			sses => {
 				// esconde carregando
 				this.buscando = false;
 
+				// Definindo local de salvamento da busca no localStorage
+				let chave:string;
+				if(this.router.url == '/home/sses/grid') {
+					chave = ChavesDeBusca.GRID;
+				} else {
+					chave = ChavesDeBusca.MAPA;
+				}
+
+				// Salvando busca no localStorage
+				localStorage.setItem(chave,JSON.stringify(busca));
+
 				// Fechando o diálogo
 				this.dialogRef.close(0);
+
 			},
 			err => {
 				// esconde carregando
